7d6360b2794251498f5ead1b3d74b2175435d4d7
Michał Niewiadomski <michal.niewiadomski@tomtom.com>
2022-01-16 20:34:22 +0100

    Add springboot template

---
+++ src/test/java/one/lucky/mastermind/MastermindApplicationTests.java
@@ -0,0 +1,13 @@
+package one.lucky.mastermind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastermindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
